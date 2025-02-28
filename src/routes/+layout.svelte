b4b86360059729180a04325d2a74f98c3c6f5cf6
mykeld74 <mike@msdwebsolutions.com>
2025-02-28 07:08:10 -0700

    Create Homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	let { data, children } = $props();
+	import '$lib/css/reset.css';
+	import '$lib/css/styles.css';
+	import logo from '$lib/images/logo.webp';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Roboto:ital,wdth,wght@0,75..100,100..900;1,75..100,100..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<header>
+	<div class="logoContainer">
+		<a href="/">
+			<img src={logo} alt="logo" />
+		</a>
+	</div>
+</header>
+
+<div>
+	{@render children()}
+</div>
+
+<style>
+	header {
+		padding: 2rem;
+	}
+	.logoContainer {
+		width: 150px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,85 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="heroContainer">
+	<div class="heroContent">
+		<h1>Nuric Productions</h1>
+		<p>Elevating Your Events with Premier Entertainment Solutions</p>
+	</div>
+</div>
+
+<style>
+	@property --textGradientX {
+		syntax: '<percentage>';
+		inherits: false;
+		initial-value: 0%;
+	}
+	@property --textGradientY {
+		syntax: '<percentage>';
+		inherits: false;
+		initial-value: 100%;
+	}
+
+	@keyframes textGradientShine {
+		0% {
+			--textGradientX: 100%;
+			--textGradientY: 0%;
+		}
+		25% {
+			--textGradientX: 100%;
+			--textGradientY: 100%;
+		}
+		50% {
+			--textGradientX: 0%;
+			--textGradientY: 100%;
+		}
+		75% {
+			--textGradientX: 0%;
+			--textGradientY: 0%;
+		}
+		100% {
+			--textGradientX: 100%;
+			--textGradientY: 0%;
+		}
+	}
+
+	.heroContainer {
+		font-family: 'Roboto', sans-serif;
+		display: grid;
+		place-items: center;
+		height: calc(100vh - 350px);
+	}
+	.heroContent {
+		text-align: right;
+		width: calc(100% - 4rem);
+		max-width: 700px;
+		h1 {
+			font-size: clamp(2.5rem, 7vw, 6rem);
+			font-weight: 800;
+			text-transform: uppercase;
+			background:
+				radial-gradient(
+					ellipse farthest-corner at right bottom,
+					#fedb37 0%,
+					#fdb931 16%,
+					#9f7928 30%,
+					#8a6e2f 40%,
+					transparent 80%
+				),
+				radial-gradient(
+					ellipse farthest-corner at var(--textGradientX) var(--textGradientY),
+					#fff 0%,
+					#f9fca1 25%,
+					#d8a520 50%,
+					#b1740e 75%,
+					#a36406 100%
+				);
+			margin: 0;
+			background-clip: text;
+			-webkit-text-fill-color: transparent;
+			color: #fff;
+			animation: textGradientShine 12s ease-in-out infinite;
+		}
+		p {
+			font-size: clamp(1.8rem, 4vw, 3.5rem);
+			text-wrap: balance;
+		}
+	}
+</style>
